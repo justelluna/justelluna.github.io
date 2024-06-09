8440108b500860ab161eed2d3754c1fc923f7781
Alexis Beckman <133524059+justelluna@users.noreply.github.com>
2024-06-09 11:12:26 -0400

    archive of cpevolved breach

---
+++ cpevolved/js/base.js
@@ -0,0 +1,15 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+{setTimeout(()=>document.body.classList.add('render'),60);const navdemos=Array.from(document.querySelectorAll('nav.links > .link'));const total=navdemos.length;const current=navdemos.findIndex(el=>el.classList.contains('link--current'));const navigate=(linkEl)=>{document.body.classList.remove('render');document.body.addEventListener('transitionend',()=>window.location=linkEl.href)};imagesLoaded('.glitch__img',{background:!0},()=>{document.body.classList.remove('loading');document.body.classList.add('imgloaded')})}
+
+}
---
+++ cpevolved/js/notice.js
@@ -0,0 +1,38 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var modal=document.getElementById('notice');var btn=document.getElementById("open");var span=document.getElementsByClassName("close")[0];btn.onclick=function(){modal.style.display="block"}
+span.onclick=function(){modal.style.display="none"}
+window.onclick=function(event){if(event.target==modal){modal.style.display="none"}}
+
+}
+/*
+     FILE ARCHIVED ON 21:14:47 Apr 12, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 13:53:50 Aug 29, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 59.368
+  exclusion.robots: 0.12
+  exclusion.robots.policy: 0.11
+  RedisCDXSource: 0.56
+  esindex: 0.01
+  LoadShardBlock: 38.061 (3)
+  PetaboxLoader3.datanode: 69.563 (4)
+  CDXLines.iter: 17.294 (3)
+  load_resource: 125.528
+  PetaboxLoader3.resolve: 35.663
+*/
